fe4a5a9b5488a8e0d08b43bbe68fcc1fd4a90b10
DonaldProgram <118350874+DonaldProgram@users.noreply.github.com>
2022-11-27 14:17:24 +0100

    Update chatBot.py

--- chatBot.py
+++ chatBot.py
@@ -3,12 +3,18 @@ import unidecode
 
 # base dans laquelle le bot va chercher la reponse
 base_Donne = {
-    "comment sa va" : "BOT- Sa va :) Merci !",
-    "bonjour"       : "BOT- Bonjour, je m'appele BOT."
-                }
+    "presentation"       : "BOT- Bonjour, je m'appele BOT. Je suis heureux de vous rencontrer.",
+    "comment sa va"      : "BOT- Sa va :) Merci !",
+    "pas appris"        : "BOT- Desoler je n'ai pas compris. Qu'auriez vous repondue"
+             }
 
 DerniereReponseBOT = "rien"
 nbMessage = 0
+
+print("Ne repond pas si trop de faute d'orthographe et ne pas mettre de point ou de virgule")
+
+
+
 
 while True:
     print("")
@@ -18,18 +24,8 @@ while True:
     texteHUMAINremake = unidecode.unidecode(texteHUMAIN)
     texteHUMAIN = texteHUMAINremake.lower()
 
-    # le bot lui reponds
-    
-    if 'bonjour' in texteHUMAIN or 'salut' in texteHUMAIN or 'comment tu t\'appele' in texteHUMAIN or 'quel est ton nom' in texteHUMAIN:
-        if DerniereReponseBOT == "rien":
-            print(base_Donne["bonjour"]) # repond 'Bonjour je m'appele BOT'
-            DerniereReponseBOT = "debut"
-        else:
-            print("BOT- Je ne pense pas que ce soit le bon moment pour que je te le dises.")
-
-
-    elif 'comment tu va' in texteHUMAIN or 'comment sa va' in texteHUMAIN or 'sa va' or 'comment tu te sens' in texteHUMAIN:
-        print(base_Donne["comment sa va"]) # repond sa va merci
-    
+    if texteHUMAIN == "bonjour" or texteHUMAIN == "bonjour " or texteHUMAIN == "salut " or texteHUMAIN == "salut" or texteHUMAIN == "comment tu t'appele" or texteHUMAIN == "comment tu t'appele " or texteHUMAIN == "quel est ton nom" or texteHUMAIN == "quel est ton nom ":
+        print(base_Donne["presentation"])
+        
     else:
-        print("BOT- Desoler, on ne ma pas encore appris a faire cela.")
+        print(base_Donne["pas appris"])
