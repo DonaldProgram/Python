6bc14ad6677e5b6d0f11ec382156053ae95ab1b3
DonaldProgram <118350874+DonaldProgram@users.noreply.github.com>
2022-12-29 14:53:18 +0100

    Update Pocket-Mon.py

--- Pocket-Mon.py
+++ Pocket-Mon.py
@@ -7,35 +7,72 @@ screen = pygame.display.set_mode((1915, 1040))
 pygame.display.set_caption("Pocket-Mon")
 #...
 
-
-# icon pokemon Menu txt 
-MenuPokemonTxt = pygame.image.load("MenuPokemonTxt.png")
-Motismadex = pygame.image.load("Motismadex.png")
+# variable pour gerer les fenetres a afficher
+fenetre = "menu"
 #...
+
+# icon pokemon 
+MenuPokemonTxt = pygame.image.load("Autre/MenuPokemonTxt.png")
+Motismadex = pygame.image.load("Autre/Motismadex.png")
+pokeballSimple = pygame.image.load("Autre/pokeballSimple.png")
+pokestop = pygame.image.load("Autre/pokestop.png")
+superball = pygame.image.load("Autre/superball.png")
+oeuf = pygame.image.load("Autre/oeuf.png")
+
+# Charger les images des pokemons
+TortueMan = pygame.image.load("Pokemon/TortueMan.png")
+GrosTasDeMorve = pygame.image.load("Pokemon/GrosTasDeMorve.png")
+Pikachu = pygame.image.load("Pokemon/Pikachu.png")
+#...
+#...
+
+# definir l'ecran du menu
+def Menu():
+    global fenetre
+    while fenetre == "menu":
+        # recuperer les touches presser
+        pressed = pygame.key.get_pressed()
+        #...
+
+        # recuperer la position de la souris
+        pos = pygame.mouse.get_pos()
+        #...
+        
+        # pouvoir fermer la fenetre
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                fenetre = "Autre"
+        #...
+        # couleur de l'écran a blanc    
+        screen.fill((255, 255, 255))
+        #...   
+
+        # afficher les icons sur l'ecran 
+        screen.blit(MenuPokemonTxt, (300, 0))
+        screen.blit(pokeballSimple, (30, 600))
+        screen.blit(pokestop, (100, 900))
+        screen.blit(superball, (600, 800))
+        # afficher les pokemons sur l'ecran
+        screen.blit(TortueMan, (600, 600))
+        screen.blit(GrosTasDeMorve, (400, 800))
+        screen.blit(Pikachu, (400, 50)) 
+        #...
+        #...
+
+        # mise a jour de l'ecran
+        pygame.display.flip()
+        #...
+
 
 
 # boucle principale
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    # recuperer la position de la souris
-    pos = pygame.mouse.get_pos()
+    # afficher la fenetre du menu
+    Menu()
     #...
 
-    # recuperer les touches pressés
-    pressed = pygame.key.get_pressed()
+    # fermer la fenetre
+    running = False
+    pygame.quit()
     #...
-
-    # couleur de l'écran a blanc    
-    screen.fill((255, 255, 255))
-    #...
-    
-    screen.blit(MenuPokemonTxt, (300, 0))
-    screen.blit(Motismadex, (20, 980))
-
-    # mise a jour de l'ecran
-    pygame.display.flip()
-    #...
-    
