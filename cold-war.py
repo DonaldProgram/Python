da2d7218115240473cf6be1f82d5324da7f2c922
DonaldProgram <118350874+DonaldProgram@users.noreply.github.com>
2023-05-19 21:42:51 +0200

    Update cold-war.py

--- cold-war.py
+++ cold-war.py
@@ -21,8 +21,8 @@ fleche_choix = font.render('->', True, (255, 0, 0))
 
 
 
-# variable choix permettant de sélectionner les différents textes
-choix = 1
+# variable choixMenu permettant de sélectionner les différents textes
+choixMenu = 1
 # ...
 
 # definir une variable delay pour patientez un peu
@@ -30,16 +30,16 @@ delay = 200
 #...
 
 # fonction pour gérer le menu
-jouer = False
+choixOnglet = 'MenuMenu'
 def Menu(pressed):
-    global texte_jouer, texte_miroir, texte_parametre, choix, last_change, jouer
+    global texte_jouer, texte_miroir, texte_parametre, choixMenu, last_change, choixOnglet
 
-    # afficher la flèche du choix
-    if choix == 1:
+    # afficher la flèche du choixMenu
+    if choixMenu == 1:
         screen.blit(fleche_choix, (750, 350))
-    if choix == 2:
+    if choixMenu == 2:
         screen.blit(fleche_choix, (725, 435))
-    if choix == 3:
+    if choixMenu == 3:
         screen.blit(fleche_choix, (670, 520))
     # ...
 
@@ -50,45 +50,107 @@ def Menu(pressed):
     screen.blit(texte_parametre, ((1920 - texte_parametre.get_width()) / 2, 520))
     # ...
 
-    # déplacer la flèche de choix si des touches sont pressées
+    # déplacer la flèche de choixMenu si des touches sont pressées
     if pressed[pygame.K_DOWN] and pygame.time.get_ticks()-last_change >= delay:
-        if choix != 3:
-            choix += 1
+        if choixMenu != 3:
+            choixMenu += 1
             last_change = pygame.time.get_ticks()
     if pressed[pygame.K_UP] and pygame.time.get_ticks()-last_change >= delay:
-        if choix != 1:
-            choix -= 1
+        if choixMenu != 1:
+            choixMenu -= 1
             last_change = pygame.time.get_ticks()
 
     # verifier si la touche entré est pressé
-    if pressed[pygame.K_RETURN] and choix == 1:
-        jouer = True
+    if pressed[pygame.K_RETURN] and choixMenu == 1:
+        choixOnglet = 'MenuJouer'
     # ...
     #...
 # ...
 
-# fonction qui gere le choix entre 1c1 ou 1cBOT
-def ChoixPartie():
-    ...
-    # verifier si le bouton choix est cliqué
+
+
+
+
+
+# fonction qui gere le choixMenu entre 1c1 ou 1cBOT
+choixPartie = 1
+font2 = pygame.font.SysFont('Courier New', 107)
+texte_jouer2 = font2.render('JOUER', True, (255, 0, 0))
+texte_1c1 = font2.render('1v1', True, (255, 0, 0))
+texte_1cBOT = font2.render('1vBOT', True, (255, 0, 0))
+font2 = pygame.font.SysFont("Courier New", 100)
+fleche_choix2 = font2.render('->', True, (255, 0, 0))
+
+choix_modes = 0
+
+def ChoixPartie():    
+    global choixPartie, texte_1c1, texte_jouer, last_change
+    # afficher les boutons du choixMenu entré 1c1 ou 1cBOT
+    screen.blit(texte_jouer2, ((1920 - texte_jouer2.get_width()) / 2, 15))
+    screen.blit(texte_1c1, ((1920 - texte_1c1.get_width()) / 2, 350))
+    screen.blit(texte_1cBOT, ((1920 - texte_1cBOT.get_width()) / 2, 450))
+
+    #...
+
+    # gerer la fleche en fonction des touches pressé
+    if pressed[pygame.K_DOWN] and pygame.time.get_ticks()-last_change >= delay:
+        if choixPartie != 2:
+            choixPartie += 1
+            last_change = pygame.time.get_ticks()
+    if pressed[pygame.K_UP] and pygame.time.get_ticks()-last_change >= delay:
+        if choixPartie != 1:
+            choixPartie -= 1
+            last_change = pygame.time.get_ticks()
+    
+    if choixPartie == 1:
+        screen.blit(fleche_choix2, (720, 350))
+    if choixPartie == 2:
+        screen.blit(fleche_choix2, (670, 450))
+    #...
+
+    # verifier si la touche entré est pressé
+    if pressed[pygame.K_RETURN] and choix == 1:
+        choixOnglet = 'MenuChoixPartie'
+    # ...
+
+#...
+
+
+
+# fonction gerant le 1c1
+joueur = pygame.Surface((22, 130))
+joueur.fill((255, 255, 255))
+
+y1 = 540-joueur.get_height()
+y2 = 540-joueur.get_height()
+
+def v1():
+    # afficher les deux joueur
+    screen.blit(joueur, (15, y1))
+    screen.blit(joueur, (1882, y2))
     #... 
 #...
 
 
 
+
 # fonction gerant les differante fonctions a appelé lors du 
 def Game():
+    
     # appeler la fonction menu au debut de la partie
-    if jouer == False:
+    if choixOnglet == 'MenuMenu':
         Menu(pressed)
     #...
 
-    # appeler la fonction du choix de la partie
-    if jouer == True:
+    # appeler la fonction du choixMenu de la partie
+    elif choixOnglet == 'MenuJouer':
         ChoixPartie()
     #...
 
+    elif choixOnglet == 'MenuChoixPartie':
+        v1()
 #...
+
 
 
 
