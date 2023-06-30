4b8ae91057e305dfc3af27529ec83129d1e26554
DonaldProgram <118350874+DonaldProgram@users.noreply.github.com>
2023-06-30 15:09:46 +0200

    Update MarsLife.py

--- MarsLife.py
+++ MarsLife.py
@@ -7,8 +7,8 @@ screen = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("MarsLife")
 #
 
-
-
+# variable pour savoir quel menu doit etre afficher 
+phase = 'MenuLancerJeu'
 
 # FONCTION DU MENU GO MARS
 # creer le vaisseau qui permet de cliquer dans le menu
@@ -35,58 +35,59 @@ rectINFO.y = (800-InfoJeu.get_height())/2+25
 #...
 
 def MenuStartGame():
+    global phase
     
-    
-    # gerer les touches presser
-    pressed = pygame.key.get_pressed()
-    
-    if pressed[pygame.K_UP]:
-        vaisseau.y -= 5
-        vaisseau.width, vaisseau.height = 15, 20
-    
-    elif pressed[pygame.K_DOWN]:
-        vaisseau.y += 5
-        vaisseau.width, vaisseau.height = 15, 20
-    
-    elif pressed[pygame.K_LEFT]:
-        vaisseau.x -= 5
-        vaisseau.width, vaisseau.height = 20, 15
-    
-    elif pressed[pygame.K_RIGHT]:
-        vaisseau.x += 5
-        vaisseau.width, vaisseau.height = 20, 15
+    # verifier si le menu doit etre afficher
+    if phase == 'MenuLancerJeu':
     #...
     
-    
-    # verifier si le vaisseau touche des textes du menu
-    # verifier si le vaisseau touche le texte du Menu "GO TO MARS"
-    if rectGOTOMARS.colliderect(vaisseau):
-        LancerLeJeu = font.render("-> EXPLORER L'INFINIE <-", 1, (255, 255, 255))
-    else:
-        LancerLeJeu = font.render("Explorer L'Infinie", 1, (255, 255, 255))
-    #...
-    if rectINFO.colliderect(vaisseau):
-        InfoJeu = font.render("-> INFO <-", 1, (255, 255, 255))
-    else:
-        InfoJeu = font.render("Info", 1, (255, 255, 255))
-    #...
+        # gerer les touches presser
+        pressed = pygame.key.get_pressed()
+        
+        if pressed[pygame.K_UP]:
+            vaisseau.y -= 5
+            vaisseau.width, vaisseau.height = 15, 20
+        
+        elif pressed[pygame.K_DOWN]:
+            vaisseau.y += 5
+            vaisseau.width, vaisseau.height = 15, 20
+        
+        elif pressed[pygame.K_LEFT]:
+            vaisseau.x -= 5
+            vaisseau.width, vaisseau.height = 20, 15
+        
+        elif pressed[pygame.K_RIGHT]:
+            vaisseau.x += 5
+            vaisseau.width, vaisseau.height = 20, 15
+        #...
+        
+        
+        # verifier si le vaisseau touche des textes du menu
+        # verifier si le vaisseau touche le texte du Menu "GO TO MARS"
+        if rectGOTOMARS.colliderect(vaisseau):
+            LancerLeJeu = font.render("-> EXPLORER L'INFINIE <-", 1, (255, 255, 255))
+            if pressed[pygame.K_SPACE]:
+                phase = 'explication'
+        else:
+            LancerLeJeu = font.render("Explorer L'Infinie", 1, (255, 255, 255))
+        #...
+        if rectINFO.colliderect(vaisseau):
+            InfoJeu = font.render("-> INFO <-", 1, (255, 255, 255))            
+        else:
+            InfoJeu = font.render("Info", 1, (255, 255, 255))
+        #...
 
-
-    # mettre l'ecran en noir
-    screen.fill((0, 0, 0))        
-    #...
-    
-    
-    # afficher les textes sur le Menu
-    screen.blit(Titre, ((1000-Titre.get_width())/2, (800-Titre.get_height())/2-300))
-    screen.blit(LancerLeJeu, ((1000-LancerLeJeu.get_width())/2, (800-LancerLeJeu.get_height())/2-50))
-    screen.blit(InfoJeu, ((1000-InfoJeu.get_width())/2, (800-InfoJeu.get_height())/2 + 25))    
-    #...
-    
-    
-    # afficher le vaisseau
-    pygame.draw.rect(screen, (255, 255, 255), vaisseau)
-    #...
+        
+        # afficher les textes sur le Menu
+        screen.blit(Titre, ((1000-Titre.get_width())/2, (800-Titre.get_height())/2-300))
+        screen.blit(LancerLeJeu, ((1000-LancerLeJeu.get_width())/2, (800-LancerLeJeu.get_height())/2-50))
+        screen.blit(InfoJeu, ((1000-InfoJeu.get_width())/2, (800-InfoJeu.get_height())/2 + 25))    
+        #...
+        
+        
+        # afficher le vaisseau
+        pygame.draw.rect(screen, (255, 255, 255), vaisseau)
+        #...
 #...
 
 
@@ -106,7 +107,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             
-
+    # mettre l'ecran en noir
+    screen.fill((0, 0, 0))        
+    #...
+        
+    
+    
+    
     # afficher le menu GO TO MARS
     MenuStartGame()
     #...
