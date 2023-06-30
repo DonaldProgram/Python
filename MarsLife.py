1c57019b5cc821e4321317b4867de47994833d72
DonaldProgram <118350874+DonaldProgram@users.noreply.github.com>
2023-06-30 11:35:23 +0200

    Update MarsLife.py

--- MarsLife.py
+++ MarsLife.py
@@ -17,19 +17,24 @@ vaisseau = pygame.Rect(150, 150, 20, 15)
 
 # creer les textes a afficher sur le Menu
 font = pygame.font.SysFont('monospace', 50)
-Titre = font.render("MarsLife", 1, (255, 255, 255))
-LancerLeJeu = font.render("Go to MARS", 1, (255, 255, 255))
+
+Titre = font.render("SYSTEME SOL-AIR", 1, (255, 255, 255))
+LancerLeJeu = font.render("Explorer L'Infinie", 1, (255, 255, 255))
 InfoJeu = font.render("Info", 1, (255, 255, 255))
 #...
 
 # creer les rects des textes du menu
 rectGOTOMARS = LancerLeJeu.get_rect()
 rectGOTOMARS.x = (1000-LancerLeJeu.get_width())/2
-rectGOTOMARS.y = (800-LancerLeJeu.get_height())/2-100
+rectGOTOMARS.y = (800-LancerLeJeu.get_height())/2-50
+
+
+rectINFO = InfoJeu.get_rect()
+rectINFO.x = (1000-InfoJeu.get_width())/2
+rectINFO.y = (800-InfoJeu.get_height())/2+25
 #...
 
-def GoMars():
-    global Titre
+def MenuStartGame():
     
     
     # gerer les touches presser
@@ -53,23 +58,29 @@ def GoMars():
     #...
     
     
+    # verifier si le vaisseau touche des textes du menu
     # verifier si le vaisseau touche le texte du Menu "GO TO MARS"
     if rectGOTOMARS.colliderect(vaisseau):
-        LancerLeJeu = font.render("-> Go to MARS <-", 1, (255, 255, 255))
+        LancerLeJeu = font.render("-> EXPLORER L'INFINIE <-", 1, (255, 255, 255))
     else:
-        LancerLeJeu = font.render("Go to MARS", 1, (255, 255, 255))
+        LancerLeJeu = font.render("Explorer L'Infinie", 1, (255, 255, 255))
     #...
-    
-        
+    if rectINFO.colliderect(vaisseau):
+        InfoJeu = font.render("-> INFO <-", 1, (255, 255, 255))
+    else:
+        InfoJeu = font.render("Info", 1, (255, 255, 255))
+    #...
+
+
     # mettre l'ecran en noir
     screen.fill((0, 0, 0))        
     #...
     
     
     # afficher les textes sur le Menu
-    
     screen.blit(Titre, ((1000-Titre.get_width())/2, (800-Titre.get_height())/2-300))
-    screen.blit(LancerLeJeu, ((1000-LancerLeJeu.get_width())/2, (800-LancerLeJeu.get_height())/2-100))
+    screen.blit(LancerLeJeu, ((1000-LancerLeJeu.get_width())/2, (800-LancerLeJeu.get_height())/2-50))
+    screen.blit(InfoJeu, ((1000-InfoJeu.get_width())/2, (800-InfoJeu.get_height())/2 + 25))    
     #...
     
     
@@ -97,7 +108,7 @@ while running:
             
 
     # afficher le menu GO TO MARS
-    GoMars()
+    MenuStartGame()
     #...
     
     
