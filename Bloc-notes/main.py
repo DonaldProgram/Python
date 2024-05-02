255fc45cf6fa1f0584f857ba09e78e678a1d2d49
DonaldProgram <118350874+DonaldProgram@users.noreply.github.com>
2024-05-02 10:08:18 +0200

    Update main.py
    Création des boutons en haut de la fenêtre dans le Bloc-notes.
    Changer la 'phase' en fonction du bouton pressé.

--- Bloc-notes/main.py
+++ Bloc-notes/main.py
@@ -11,10 +11,10 @@ font = pygame.font.SysFont('Arial', 13)
 # nom du fichier
 nom_fichier = 'Sans Titre'
 
-
+# variable servant a savoir la phase en cours
+phase = 'Bloc-notes-ecriture'
 
 # Gerer ce que font tous les boutons du jeu
-
 # créer tous les textes de la fenetre
 x_bouton_rect = 0 # coordonné x des rects des boutons
 
@@ -55,6 +55,8 @@ texte_aide_rect.x = x_bouton_rect
 
 # fonction servant a gerer tous les boutons du jeu
 def bouton():
+    global phase
+
     # Afficher tous les textes des boutons
     for texte, rect in [(texte_fichier, texte_fichier_rect),
                        (texte_edition, texte_edition_rect),
@@ -62,10 +64,47 @@ def bouton():
                        (texte_affichage, texte_affichage_rect),
                        (texte_calendrier, texte_calendrier_rect),
                        (texte_aide, texte_aide_rect)]:
+        # si collision avec la souris
         if rect.collidepoint(pos):
             pygame.draw.rect(screen, (0, 120, 215), rect)
+            # si clique sur un bouton
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if rect == texte_fichier_rect:
+                        print('FICHIER')
+                        phase = 'Bloc-notes-fichier'
+
+                    elif rect == texte_edition_rect:
+                        print('EDITION')
+                        phase = 'Bloc-notes-edition'
+
+                    elif rect == texte_format_rect:
+                        print('FORMAT')
+                        phase = 'Bloc-notes-format'
+
+                    elif rect == texte_affichage_rect:
+                        print('AFFICHAGE')
+                        phase = 'Bloc-notes-affichage'
+
+                    elif rect == texte_calendrier_rect:
+                        print('CALENDRIER')
+                        phase = 'Bloc-notes-calendrier'
+
+                    elif rect == texte_aide_rect:
+                        print('AIDE')
+                        phase = 'Bloc-notes-aide'
+
+        # afficher le bouton en haut de l'écran
         screen.blit(texte, rect.topleft)
 
+    # afficher la delimitation entre les boutons et la zone de texte
+    pygame.draw.rect(screen, (255, 255, 255), (0, texte_fichier.get_height()+1, screen.get_width(), 1))
+
+
+
+
+# mettre le nb de FPS
+clock = pygame.time.Clock()
 
 # boucle du jeu
 running = True
@@ -76,17 +115,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # recuperer la position de la souris
-    pos = pygame.mouse.get_pos()
-
     # couleur du BG
     screen.fill((30, 30, 30))
+
+    # recuperer la position de la souris
+    pos = pygame.mouse.get_pos()
 
     # changer le nom de la fenetre
     pygame.display.set_caption(f'{nom_fichier} - Bloc-notes')
 
     # afficher tous les boutons a l'écran
     bouton()
+
+    # mettre le nb de FPS
+    clock.tick(60)
 
     # mettre a jour la fenetre
     pygame.display.flip()
